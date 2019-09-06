ce4221392d5f35f0ca2382ce719d2afd61b1b70b
root <root@localhost.localdomain>
2019-09-06 17:01:59 -0400

    Refactored print statements to store in variables

--- active_info.py
+++ active_info.py
@@ -7,15 +7,23 @@ from pprint import pprint
 nm_scan = nmap.PortScanner()
 nm_scanner = nm_scan.scan(sys.argv[1],'80',arguments='-O')
 
+host_status = 'The host is: {}\n'.format(nm_scanner['scan'][sys.argv[1]]['status']['state'])
+port_status = 'The port 80 is: {}\n'.format(nm_scanner['scan'][sys.argv[1]]['tcp'][80]['state'])
+scanning_method = 'The scanning method is: {}\n'.format(nm_scanner['scan'][sys.argv[1]]['tcp'][80]['reason'])
+try:
+    guess_os = 'There is {} percent chance that the host is running {}'.format(nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['accuracy'],nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['name'])
+except IndexError:
+    guess_os = 'No OS info gathered'
+
 #Pretty Print Full Scan
 pprint(nm_scanner)
 
 #Summary Print Block
 print('Summary: \n')
-print('The host is: {}\n'.format(nm_scanner['scan'][sys.argv[1]]['status']['state']))
-print('The port 80 is: {}\n'.format(nm_scanner['scan'][sys.argv[1]]['tcp'][80]['state']))
-print('The scanning method is: {}\n'.format(nm_scanner['scan'][sys.argv[1]]['tcp'][80]['reason']))
-try:
-    print('There is {} percent chance that the host is running {}'.format(nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['accuracy'],nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['name']))
-except IndexError:
-    print('No OS info gathered')
+print(host_status)
+print(port_status)
+print(scanning_method)
+print(guess_os)
+
+#if len(sys.arguments) == 3:
+#    with open("%s.txt"%sys.argv[2], 'w') as f:
